7b32feaa5c9ae3aac005d2086de64133dc0bffab
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-11-04 14:42:25 -0600

    Adding more verbose initial example

--- wyrm.go
+++ wyrm.go
@@ -3,11 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	name := flag.String("name", "world", "Then name to greet.")
+	name := flag.String("name", "wyrm", "The name to greet.")
 	flag.Parse()
+
 	fmt.Printf("Hello, %s!\n", *name)
-	fmt.Printf("Memory Address, %p!\n", name)
+	if flag.Arg(0) == "list" {
+		files, _ := os.Open(".")
+		defer files.Close()
+
+		fileInfo, _ := files.Readdir(-1)
+		for _, file := range fileInfo {
+			fmt.Println(file.Name())
+		}
+	}
+
 }
